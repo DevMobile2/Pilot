2debdf467db61188952ac40ccf903cf509f8218e
ramesh <rameshmkll99@gmail.com>
2016-05-10 12:25:52 +0530

    made UI
    created ui

--- Pilot/app/src/main/java/com/amzur/pilot/ViewItemDetailsActivity.java
+++ Pilot/app/src/main/java/com/amzur/pilot/ViewItemDetailsActivity.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,15 +15,28 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.facebook.login.LoginManager;
+import com.amzur.pilot.utilities.PreferenceData;
+
+/**
+ * This class displays item details of a selected item.
+ */
 
 public class ViewItemDetailsActivity extends AppCompatActivity {
-    //selected item imageview.
-    ImageView imageviewItem;
+    //selected item image view.
+    ImageView ivItem;
     //to display company name
-    TextView item_company_name;
+    TextView tvCompanyName;
     //Toolbar of the Activity.
     Toolbar toolbar;
+    //collapsingToolbarLayout of the activity.
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,51 +47,51 @@ public class ViewItemDetailsActivity extends AppCompatActivity {
         initializeUIElements();
         getDataFromPreviousActivity();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(onClickListener);
     }
 
     /**
      * This method initializes all the ui elements.
      */
     private void initializeUIElements() {
-        imageviewItem = (ImageView) findViewById(R.id.imageview_item);
-     item_company_name= (TextView) findViewById(R.id.item_company_name);
-        Display display = getWindowManager().getDefaultDisplay();
-        int imageview_height = display.getHeight();
-        Log.i("height",imageview_height+"");
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, imageview_height/2);
-        imageviewItem.setLayoutParams(layoutParams);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        ivItem = (ImageView) findViewById(R.id.imageview_item);
+        tvCompanyName = (TextView) findViewById(R.id.tvCompanyName);
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        int height = PreferenceData.getScreenHeight(ViewItemDetailsActivity.this);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, height / 2);
+        ivItem.setLayoutParams(layoutParams);
 
     }
 
+    /**
+     * This method reads item details  from the previous activity.
+     */
     public void getDataFromPreviousActivity() {
-        Bundle b = getIntent().getBundleExtra("item_details");
+        Bundle b = getIntent().getBundleExtra("itemDetails");
         if (b != null) {
-            if (b.getInt("image_id") != 0)
-                imageviewItem.setImageResource(b.getInt("image_id"));
-            if(b.getString("comapnay_name")!=null)
-               toolbar.setTitle(b.getString("comapnay_name"));
-
-
-
+            if (b.getInt("imageId") != 0)
+                ivItem.setImageResource(b.getInt("imageId"));
+            if (b.getString("companyName") != null) {
+                collapsingToolbarLayout.setTitle(b.getString("companyName"));
+                collapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(ViewItemDetailsActivity.this, R.color.blue_app));
+            }
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        menu.add(Menu.NONE,R.id.button,Menu.NONE,"Logout").setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menu.add(Menu.NONE, R.id.button, Menu.NONE, "Logout").setIcon(R.drawable.ic_power_settings_new_white_24dp).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.button)
-            LoginManager.getInstance().logOut();
+        if (item.getItemId() == R.id.button)
+            PreferenceData.signOut(ViewItemDetailsActivity.this);
+        if (item.getItemId() == android.R.id.home)
+            finish();
         return super.onOptionsItemSelected(item);
     }
 }
